d087bc751f414b757961ecce05b6017982decca5
gundaajaykumar <38875061+gundaajaykumar@users.noreply.github.com>
2018-05-22 10:00:28 +0530

    up,

--- media.py
+++ media.py
@@ -14,3 +14,35 @@ class Movie():
 
     def show_trailer(self):
         webbrowser.open(self.trailer_youtube_url)
+#!/usr/bin/env python
+2
+import webbrowser
+3
+# take the webbrowser to run this python file
+4
+​
+5
+​
+6
+class Movie():
+7
+    # define a class and take an object self.
+8
+    def __init__(self, movie_name,
+9
+                 storyline_movie, movieposter_image, movietrailer_youtube):
+10
+                self.title = movie_name
+11
+                self.storyline = storyline_movie
+12
+                self.poster_image_url = movieposter_image
+13
+                self.trailer_youtube_url = movietrailer_youtube
+14
+​
+15
+    def show_trailer(self):
+16
+        webbrowser.open(self.trailer_youtube_url)
+17
