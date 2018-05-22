7fa07181b957ae6551b57842398312d4314fd329
gundaajaykumar <38875061+gundaajaykumar@users.noreply.github.com>
2018-05-22 09:27:20 +0530

    updated

--- entertainment_center.py
+++ entertainment_center.py
@@ -1,6 +1,8 @@
 #!/usr/bin/env python
 import media
+# here imported the media.py file.
 import fresh_tomatoes
+# here imported the fresh_tomatoes.py file.
 Jakkana = media.Ajay("Jakkana", "action and comedy",
                      "https://bit.ly/2IV9sDd",
                      "https://www.youtube.com/embed/RaO7Fyc1RMQ")
@@ -19,6 +21,9 @@ Tintin = media.Ajay("Tintin", "advantures",
 Ghajini = media.Ajay("Ghajini", "revange",
                      "https://bit.ly/2GyUwFO",
                      "https://youtu.be/_I0xx8Oj3Ww")
+''' here we connected the class Ajay in media.py file
+and taken the movies information in tuple format'''
 movies = [Jakkana, Paddanandipremalomari,
           Titanic, Shaolinsoccer, Tintin, Ghajini]
 fresh_tomatoes.open_movies_page(movies)
+# in fresh_tomatoes we open the open_movie_page with above movies.
